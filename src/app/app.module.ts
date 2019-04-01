2f982a8c35d0f891176898132dc6a632f0d4e394
XiongXuan <me@xiongxuan.me>
2019-04-01 23:21:27 +0800

    添加路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,9 @@ import { NZ_ICON_DEFAULT_TWOTONE_COLOR, NZ_ICONS } from 'ng-zorro-antd';
 // 引入你需要的图标，比如你需要 fill 主题的 AccountBook Alert 和 outline 主题的 Alert，推荐 ✔️
 import { BookFill, TrophyFill, PieChartFill, HomeFill } from '@ant-design/icons-angular/icons';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const icons: IconDefinition[] = [ HomeFill, BookFill, TrophyFill, PieChartFill ];
 
@@ -26,7 +29,9 @@ registerLocaleData(zh);
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +39,8 @@ registerLocaleData(zh);
     NgZorroAntdModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [
     { provide: NZ_I18N, useValue: zh_CN },
