c6f1c66d26f78a25e4c3c418723137efb854df10
XiongXuan <me@xiongxuan.me>
2019-05-07 01:45:28 +0800

    新增 页脚添加备案号 更换 网站图标 更换 网站标题

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'OnlineJudgeFE';
+  title = 'XOJ - 算法题目';
+
+  public constructor(private titleService: Title) {
+    this.titleService.setTitle(this.title)
+  }
 }
